a4cd5ceab35bed40a08ea4243675046813b4400a
DeepsoftwareEng <118848144+DeepsoftwareEng@users.noreply.github.com>
2023-04-21 22:08:14 +0700

    Update Sql

--- REF/CCPTPM.sql
+++ REF/CCPTPM.sql
@@ -34,6 +34,10 @@ create table Attend(
 create table Attendance(
 	id int not null,
 	d_m date not null,
+	checkin date,
+	checkout date,
+	workingHour float,
+	daySalary float,
 	primary key(id, d_m),
 	constraint fk_Attendance_WorkerList foreign key (id) References WorkerList(id),
 	constraint fk_Attendance_Attend foreign key (d_m) references Attend(d_m)
@@ -57,6 +61,8 @@ Select* from UsedEmail
 
 Insert into Faculty values ('CNTT01','Cong nghe thong tin 1'),
 						   ('CSKH01','Cham soc khanh hang 1')
+insert into Account values ('admin1', 'admin1', 1, 'deepit2507@gmail.com', 'admin1', 'CNTT01'),
+							('staff1', 'staff1', 0, 'minhvt223@gmail.com', 'staff1', 'CSKH01')
 Insert into WorkerList values(1, 'Nguyen Hung', '2002-07-25', 'Hung01', 'CNTT01'),
 							 (2, 'Nguyen Phuc', '2002-05-27', 'Phuc02', 'CSKH01')
 
@@ -65,3 +71,19 @@ Insert into Attend values ('2023-04-03'),
 insert into Attendance values (1, '2023-04-03'),
 							  (2, '2023-04-03'),
 							  (1, '2023-04-04')
+Select Count(1) from Account where username ='admin1' and passwords='admin1'
+Select count(1) from Account inner join Faculty on Account.fid = Faculty.id_faculty where id_faculty = 'CNTT01' and gmail='deepit2507@gmail.com' and username='admin1'
+
+DECLARE @StartDate date = '2022-02-01';
+DECLARE @EndDate date = '2022-02-28';
+
+WHILE @StartDate <= @EndDate
+BEGIN
+    INSERT INTO Attendance
+    VALUES (1, @StartDate);
+
+    SET @StartDate = DATEADD(day, 1, @StartDate);
+END
+SELECT d_m
+FROM Attendance
+WHERE MONTH(d_m) = 2 AND id=1
